c7d32d4a9c537adbddc59befbfae98758fc1ed0e
Aureen <yukiansff@gmail.com>
2021-06-09 20:16:39 +0200

    User can update their avatar.

--- src/components/settings/Avatar.jsx
+++ src/components/settings/Avatar.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
 
 function Avatar() {
+  const handleChange = async (e) => {
+    const formData = new FormData();
+    formData.append("image", e.target.files[0]);
+
+    const res = await fetch(
+      `${process.env.REACT_APP_URL}/users/${
+        JSON.parse(localStorage.getItem("user"))._id
+      }/avatar`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("jwt")}`
+        },
+        body: formData,
+      }
+    );
+  };
+
   return (
     <div>
-      
+      <label htmlFor="avatar">
+        <input type="file" id="avatar" name="avatar" onChange={handleChange} />
+      </label>
     </div>
-  )
+  );
 }
 
-export default Avatar
+export default Avatar;
--- src/routes/Settings.jsx
+++ src/routes/Settings.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Email from "../components/settings/Email";
 import Password from "../components/settings/Password";
 import Username from "../components/settings/Username";
+import Avatar from "../components/settings/Avatar";
 
 function Settings() {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
@@ -20,9 +21,13 @@ function Settings() {
           </div>
 
           <div>
-            {user.username}
-            Username
+            Username: {user.username}
             <Username />
+          </div>
+
+          <div>
+            Avatar
+            <Avatar />
           </div>
         </>
       )}
