f25a12e2a2c104777e2294375279c94b77962f3d
Aureen <yukiansff@gmail.com>
2021-06-10 12:19:04 +0200

    Friends are listed and can be removed.

--- src/components/friends/All.jsx
+++ src/components/friends/All.jsx
@@ -1,12 +1,56 @@
-import React from 'react'
-import useFetch from "../../hooks/useFetch";
+import React, { useState, useEffect } from "react";
 
 function All() {
+  const [friends, setFriends] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    // Fetch friends
+    (async () => {
+      const res = await fetch(
+        `${process.env.REACT_APP_URL}/users/${
+          JSON.parse(localStorage.getItem("user"))._id
+        }/friends`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const json = await res.json();
+      if (json.error) return setError(json.error);
+      setFriends(json);
+      console.log(json);
+    })();
+  }, []);
+
+  const remove = async (friendshipId) => {
+    await fetch(`${process.env.REACT_APP_URL}/friends/${friendshipId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <ul>
+      {friends.map((friend) => (
+        <li key={friend._id}>
+          {friend.recipient._id === JSON.parse(localStorage.getItem("user"))._id ? (
+            <>{friend.sender.username}</>
+          ) : (
+            <>{friend.recipient.username}</>
+          )}
+          <button type="button" onClick={() => remove(friend._id)}>
+            Remove Friend
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-export default All
+export default All;
