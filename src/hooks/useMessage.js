0925340a32d4e181a658d67b66e7c115638a078d
Aurélie <nn.aurelie@hotmail.com>
2021-06-15 13:17:30 +0200

    Moved messages logic (state, socket listeners) in its own hook.

---
+++ src/hooks/useMessage.js
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+import socket from "../socket/socket";
+
+function useMessage(url) {
+  const [messages, setMessages] = useState([]);
+
+  // Load messages
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        },
+      });
+      const json = await res.json();
+      if (!json.error) setMessages(json);
+    })();
+  }, [url]);
+
+  // Set up socket listeners
+  const handleInsert = (newMessage) => {
+    setMessages([...messages, newMessage.document]);
+  };
+
+  const handleUpdate = (updated) => {
+    // Doesn't update messages if we are the author
+    // Messages was already updated instantaneously by the form handler.
+    if (
+      updated.document.author._id ===
+      JSON.parse(localStorage.getItem("user"))._id
+    )
+      return;
+
+    setMessages((prev) => {
+      const update = [...prev].map((message) => {
+        return message._id.toString() === updated.document._id
+          ? updated.document
+          : message;
+      });
+      return update;
+    });
+  };
+
+  const handleDelete = (deleted) => {
+    if (
+      messages.findIndex((message) => message._id === deleted.document._id) !==
+      -1
+    ) {
+      setMessages((prev) =>
+        prev.filter((message) => message._id !== deleted.document._id)
+      );
+    }
+  };
+
+  useEffect(() => {
+    socket.on("insert", (document) => {
+      handleInsert(document);
+    });
+    return () => socket.off("insert");
+  }, [messages]);
+
+  useEffect(() => {
+    socket.on("update", (document) => {
+      handleUpdate(document);
+    });
+    return () => socket.off("update");
+  }, [messages]);
+
+  useEffect(() => {
+    socket.on("delete", (document) => {
+      handleDelete(document);
+    });
+    return () => socket.off("delete");
+  }, [messages]);
+
+  return {
+    messages,
+    setMessages,
+  };
+}
+
+export default useMessage;
--- src/routes/Conversation.jsx
+++ src/routes/Conversation.jsx
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Form from "../components/chat/Form";
 import Messages from "../components/chat/Messages";
-import socket from "../socket/socket";
+import useMessage from "../hooks/useMessage";
 
 function Conversation({ match }) {
-  const [messages, setMessages] = useState([]);
   const [conversation, setConversation] = useState();
   const [editing, setEditing] = useState(false);
+  const { messages, setMessages } = useMessage(
+    conversation &&
+      `${process.env.REACT_APP_URL}/conversations/${conversation._id}/messages`
+  );
 
   // Load conversation
   useEffect(() => {
@@ -52,80 +55,6 @@ function Conversation({ match }) {
       setConversation(conversation);
     })();
   }, [match]);
-
-  // Get messages
-  useEffect(() => {
-    if (!conversation) return;
-    (async () => {
-      const res = await fetch(
-        `${process.env.REACT_APP_URL}/conversations/${conversation._id}/messages`,
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("jwt")}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const json = await res.json();
-      if (!json.error) setMessages(json);
-    })();
-  }, [conversation]);
-
-  // Set up socket listeners
-  const handleInsert = (newMessage) => {
-    setMessages([...messages, newMessage.document]);
-  };
-
-  const handleUpdate = (updated) => {
-    // Doesn't update messages if we are the author
-    // Messages was already updated instantaneously by the form handler.
-    if (
-      updated.document.author._id ===
-      JSON.parse(localStorage.getItem("user"))._id
-    )
-      return;
-
-    setMessages((prev) => {
-      const update = [...prev].map((message) => {
-        return message._id.toString() === updated.document._id
-          ? updated.document
-          : message;
-      });
-      return update;
-    });
-  };
-
-  const handleDelete = (deleted) => {
-    if (
-      messages.findIndex((message) => message._id === deleted.document._id) !==
-      -1
-    ) {
-      setMessages((prev) =>
-        prev.filter((message) => message._id !== deleted.document._id)
-      );
-    }
-  };
-
-  useEffect(() => {
-    socket.on("insert", (document) => {
-      handleInsert(document);
-    });
-    return () => socket.off("insert");
-  }, [messages]);
-
-  useEffect(() => {
-    socket.on("update", (document) => {
-      handleUpdate(document);
-    });
-    return () => socket.off("update");
-  }, [messages]);
-
-  useEffect(() => {
-    socket.on("delete", (document) => {
-      handleDelete(document);
-    });
-    return () => socket.off("delete");
-  }, [messages]);
 
   return (
     <>
